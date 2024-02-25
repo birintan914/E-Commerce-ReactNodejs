978e4568a09fd3a650a8b25ea8c598d649221951
Akelan K <ak@Akelans-MacBook-Pro.local>
2024-02-24 19:48:21 -0500

    SETUP router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './navbar/Navbar';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path='/' element= {<h1>Product Component</h1>}/>
+          <Route path='/add' element= {<h1>Add Product Component</h1>}/>
+          <Route path='/update' element= {<h1>Update Component</h1>}/>
+          <Route path='/logout' element= {<h1>Logout</h1>}/>
+          <Route path='/profile' element= {<h1>Profile</h1>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
