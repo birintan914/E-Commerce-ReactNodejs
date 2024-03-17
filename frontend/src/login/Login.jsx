c77ce321dc4f4d199ae368b18e485dab13a4c2e5
birintan914 <birintancs@gmail.com>
2024-03-17 17:25:58 -0400

    Add login feature

--- frontend/src/login/Login.jsx
+++ frontend/src/login/Login.jsx
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = ()=> {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+    
+    const handleLogin = async ()=>{
+        //console.log(email, password)
+        let result = await fetch("http://localhost:5000/login", {
+            method: 'post',
+            body: JSON.stringify({email, password}),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        result = await result.json();
+        console.log(result);
+        if (!result.name) {
+            alert("Incorrect Login");
+        } else {
+            localStorage.setItem('user', JSON.stringify(result));
+            navigate('/')
+        }
+        
+    }
+
     return (
-        <div>
+        <div className="container">
             <h1>Login</h1>
+            <div className="inputBox">
+                <label htmlFor="email">Enter Email</label>
+                <input type="text" id="email" placeholder='Email' value={email} 
+                onChange={(e) => setEmail(e.target.value)}/>
+            </div>
+            <div className="inputBox">
+                <label htmlFor="password">Enter Password</label>
+                <input type="password" id="password" placeholder='Password' value={password}
+                onChange={(e) => setPassword(e.target.value)}/>
+            </div>
+            <button className="button" onClick={handleLogin}>Submit</button>
+            
+            
         </div>
     )
 }
--- frontend/src/navbar/Navbar.jsx
+++ frontend/src/navbar/Navbar.jsx
@@ -17,7 +17,14 @@ const Navbar = () => {
             <li><Link to='/add'>Add Products</Link></li>
             <li><Link to='/update'>Update Products</Link></li>
             <li><Link to='/profile'>Profile</Link></li>
-            <li>{auth ? <Link to='/signup' onClick={logout}>Logout</Link> : <Link to='/signup'>Sign Up</Link>}</li>
+            {auth ? 
+            <li><Link to='/signup' onClick={logout}>Logout</Link></li> : 
+            <>
+            <li><Link to='/signup'>Sign Up</Link></li>
+            <li><Link to='/login'>Login</Link></li> 
+            </>
+            }
+            {/* <li>{auth ? <Link to='/signup' onClick={logout}>Logout</Link> : <Link to='/signup'>Sign Up</Link>}</li> */}
         </ul>
     </div>
   )
